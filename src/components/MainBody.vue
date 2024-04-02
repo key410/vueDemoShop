8fb5b7214aadf4931e2656126547df242905d043
key410 <yuumiya5@gmail.com>
2024-04-02 11:28:23 +0900

    img相対パス化

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -4,12 +4,12 @@ import Item from "./Item.vue";
 
 var id = 0;
 var Items = ref([
-    {id: id++, itemName:"珈琲", imagePath:"/image/coffee.jpeg", price:500},
-    {id: id++, itemName:"紅茶", imagePath:"/image/tea.jpg", price:500},
-    {id: id++, itemName:"花", imagePath:"/image/flower.jpg", price:800},
-    {id: id++, itemName:"ビール", imagePath:"/image/beer.jpg", price:700},
-    {id: id++, itemName:"兎", imagePath:"/image/rabi.jpg", price:36000},
-    {id: id++, itemName:"フレーバーティー", imagePath:"/image/flavortea.jpg", price:550}
+    {id: id++, itemName:"珈琲", imagePath:"./image/coffee.jpeg", price:500},
+    {id: id++, itemName:"紅茶", imagePath:"./image/tea.jpg", price:500},
+    {id: id++, itemName:"花", imagePath:"./image/flower.jpg", price:800},
+    {id: id++, itemName:"ビール", imagePath:"./image/beer.jpg", price:700},
+    {id: id++, itemName:"兎", imagePath:"./image/rabi.jpg", price:36000},
+    {id: id++, itemName:"フレーバーティー", imagePath:"./image/flavortea.jpg", price:550}
 ])
 </script>
 
